91b18719a3e4a89c8ad5c80c6379ee88d0846949
Gaspar Carraro <gaspar.carraro@gmail.com>
2021-05-29 12:34:40 -0300

    Screen

--- components/Screen/Screen.js
+++ components/Screen/Screen.js
@@ -1,6 +1,5 @@
 import React, { useEffect } from 'react'
 import { View, ImageBackground } from 'react-native'
-import GridRow from '../Grid/GridRow';
 
 export default function Screen({header, body}) {
     return <View style={{display:"flex", flex:1,flexDirection:"column"}}>
@@ -9,70 +8,19 @@ export default function Screen({header, body}) {
     </View>
 }
 
-/**
- * Los componentes del header seran centrados vertical y horizontalmente por defecto.
- * 
- * Se asume que siempre se pasara un header. Este tendra 2 espacios asignados por defecto.
- * En el caso de que no se pasen parametros de body o footer se creara una vista vacia con 2 espacios debajo del header para mantener siempre el mismo tamaño del header.
- * En caso de que se pase un body y no un footer, este tendra 2 espacios asignados. En caso de que se pase un footer y no un body lo mismo.
- * En el caso de que se pase body y footer se le asignara un carto de espacio a cada uno.
- */
-function ScreenHeader({leftHeader, midHeader, rightHeader, body, leftFooter, midFooter, rightFooter}) {
-    const headerIsDefined = leftHeader || midHeader || rightHeader
-    const bodyIsDefined = body
-    const footerIsDefined = leftFooter || midFooter || rightFooter
-
-    let header = undefined
-    if (headerIsDefined) {
-        header = <GridRow cells={[
-            {size: 1, component: leftHeader}, 
-            {size: 3, component: midHeader}, 
-            {size: 1, component: rightHeader}]} 
-            style={{flex:2, alignItems: "center"}}
-        />
-    }
-
-    const spacesOfHeaderBottom = bodyIsDefined && footerIsDefined ? 1 : 2
-
-    let mid = undefined
-    if (bodyIsDefined) {
-        mid = <View style={{flex: spacesOfHeaderBottom}}>
-            {body}
-        </View>
-    }
-
-    let footer = undefined
-    if (footerIsDefined) {
-        footer = <GridRow cells={[
-            {size: 1, component: leftFooter}, 
-            {size: 3, component: midFooter}, 
-            {size: 1, component: rightFooter}]} 
-            style={{flex:spacesOfHeaderBottom}}
-        />
-    }
-
-    let emptyView = undefined
-    if (!(bodyIsDefined || footerIsDefined)) {
-        emptyView = <View style={{flex: 2}}>
-            </View>
-    }
-
-
+function ScreenHeader({children}) {
     return <View style={{display: "flex", flex: 1, flexDirection:"column"}}>
         <ImageBackground source={require("../../assets/Header.jpg")} style={{resizeMode: "cover", flex:1}}>
-            {/*Header*/}
-            {header}
-            {emptyView}
-            {/*Body*/}
-            {mid}
-            {/*Footer*/}
-            {footer}
+            {children}
         </ImageBackground>
     </View>
 }
 
-function ScreenBody() {
-    return <View style={{flex:3, marginTop:-22, borderTopLeftRadius: 22, borderTopRightRadius: 22, backgroundColor: "white"}}></View>
+function ScreenBody({children, size}) {
+    
+    return <View style={{flex:3, marginTop:-22, borderTopLeftRadius: 22, borderTopRightRadius: 22, backgroundColor: "white"}}>
+        {children}
+    </View>
 }
 
 
