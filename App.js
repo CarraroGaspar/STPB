55a9081c564f3a457d06ba3e41a805152385079f
Gaspar Carraro <gaspar.carraro@gmail.com>
2021-08-15 11:04:45 -0300

    Arreglo screenbody y grillas

--- App.js
+++ App.js
@@ -1,36 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import React, { useState } from 'react';
-import { StyleSheet, Text, View, TouchableOpacity, TextInput, FlatList } from 'react-native';
-import Screen, { ScreenHeader, ScreenBody } from './components/Screen/Screen'
-import firebase from './services/firebase'
-import {Grid, Cell} from 'react-native-prueba-gaspar'
-import {Title} from './components/Font/Fonts'
+import React from 'react'
+import { View, Text } from 'react-native'
+import Screen, { ScreenBody, ScreenHeader } from './components/Screen/Screen'
+import { Grid, Cell } from 'react-native-simple-layout-grid'
 
 export default function App() {
-  return <View style={{display:"flex", flexDirection:"column", flex:1}}>
-    <Screen
-      header={<ScreenHeader>
-        <Grid direction="column" cells={[
-          <Grid direction="row" cells={[
-            <Cell size={1} centerVerticalAndHorizontal key={1}><Text>Back</Text></Cell>,
-            <Cell size={3} centerVerticalAndHorizontal key={2}><Title>Detalles</Title></Cell>,
-            <Cell size={1} key={3}></Cell>
-          ]}/>,
-          <Cell size={1}><View></View></Cell>
-        ]}/>
-      </ScreenHeader>}
-      body={<ScreenBody></ScreenBody>}
-    >
-    </Screen>
-    <StatusBar style="light" />
-  </View>
+  return <Screen 
+    body={<ScreenBody>
+      <Grid cells={[
+        <Cell key={1} ><Text>Hola</Text></Cell>
+      ]}></Grid>
+    </ScreenBody>} 
+    header={<ScreenHeader></ScreenHeader>} 
+  />
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- screens/Home/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { View, Text, FlatList } from 'react-native'
-import firebase from '../../services/firebase'
-
-export default function HomeScreen() {
-    <View>
-        
-    </View>
-}
